9378725b2d9864a06f5015a24e367343bca2f858
naeemedmo <naeeme@xal.no>
2023-07-02 03:36:02 +0200

    add part 2 day 1

--- 2022/day1/src/main.rs
+++ 2022/day1/src/main.rs
@@ -13,18 +13,20 @@ fn main() {
     let session_cookie = build_cookie(&cookie);
 
     let content = get_input(&input_url, &session_cookie);
-    let mut elf = Vec::new();
+    let mut elves = Vec::new();
     let mut sum_calories: i32 = 0;
 
     for line in content.lines() {
         if line.is_empty() {
-            elf.push(sum_calories);
+            elves.push(sum_calories);
             sum_calories = 0
         } else {
             sum_calories += line.parse::<i32>().unwrap();
         }
     }
-    let max_value = *elf.iter().max().unwrap();
-    let index = elf.iter().position(|&r| r == max_value).unwrap();
+    let max_value = *elves.iter().max().unwrap();
+    let index = elves.iter().position(|&r| r == max_value).unwrap();
     println!( "Elf number {} with max calories {}", index, max_value );
+    elves.sort_by(|a, b| b.cmp(a));
+    println!( "Sum top 3 elves {}", elves[0] + elves[1] + elves[2] );
 }
